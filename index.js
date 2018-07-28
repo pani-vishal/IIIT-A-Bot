8ced4253641121536e4326906ebb7043cbe42c60
Vishal <panivishal17@gmail.com>
2018-07-28 14:15:44 +0530

    add logic

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 var college_ranks = [
   {
-      "preference" : 1,
-      "name" : "IIT Delhi",
-      "closing_rank_CSE" : 120,
+      "name" : "IIIT Allahabad",
+      "category" : {
+        "GEN" : {
+          "closing_rank_IT" : 6259,
+          "closing_rank_ECE" : 10782
+        },
+        "SC" : {
+          "closing_rank_IT" : 1958,
+          "closing_rank_ECE" : 2719
+        },
+        "ST" : {
+          "closing_rank_IT" : 1600,
+          "closing_rank_ECE" : 2400
+        },
+        "OBC" : {
+          "closing_rank_IT" : 2580,
+          "closing_rank_ECE" : 3600
+        }
+      }
   },
   {
-      "preference" : 2,
-      "name" : "IIT Bombay",
-      "closing_rank_CSE" : 60
-  },
+    "name" : "NIT Allahabad",
+    "category" : {
+      "GEN" : {
+        "closing_rank_IT" : 6260,
+        "closing_rank_ECE" : 7889
+      },
+      "SC" : {
+        "closing_rank_IT" : 2958,
+        "closing_rank_ECE" : 3719
+      },
+      "ST" : {
+        "closing_rank_IT" : 2600,
+        "closing_rank_ECE" : 3400
+      },
+      "OBC" : {
+        "closing_rank_IT" : 3580,
+        "closing_rank_ECE" : 4600
+      }
+    }
+  }
 ]
 
 
@@ -36,16 +68,19 @@ restService.post("/echo", function(req, res) {
   
   var category = req.body.queryResult.parameters.category;
 
-  var college_array = []
+  var speech = "Your rank is "+ rank + " and your category is " + category + ". Based on that you can choose "
 
   for(var i = 0; i < college_ranks.length; i++){
-    if(rank <= college_ranks[i]["closing_rank_CSE"]){
-      college_array.push(college_ranks[i]["name"]);
+    if(rank <= college_ranks[i].category[category]["closing_rank_IT"]){
+      speech += college_ranks[i]["name"] + "IT, ";
     }
+    if(rank <= college_ranks[i].category[category]["closing_rank_ECE"]){
+      speech += college_ranks[i]["name"] + "ECE, ";
+    }  
   }
 
 
-  var speech = "Your rank is "+ rank + " and your category is " + category + ". You can try these colleges: " + college_array.toString();
+  // var speech = "Your rank is "+ rank + " and your category is " + category + ". You can try these colleges: " + college_array.toString();
   return res.json({
     "fulfillmentMessages": [{"text": {"text": [speech]}}],
     "source":""
